9e92e7646a245a00f17b87a14889ddbd83d766fd
32163555 <dlwnstjr0310@gmail.com>
2024-11-21 15:15:08 +0900

    Initial commit

---
+++ src/test/kotlin/com/study/fcboard/FcBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.fcboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FcBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
